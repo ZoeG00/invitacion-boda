29d5355542e3a57f089684430e9dcfd566b2e51e
zoeg00 <zoeguzman.ok@gmail.com>
2025-03-03 13:31:28 -0300

    lala

--- components/despedida.jsx
+++ components/despedida.jsx
@@ -3,12 +3,15 @@ import foto from "../src/assets/carrousel/c3.webp";
 export default function Despedida() {
   return (
     <div className="flex justify-center items-center bg-armygreen">
-      <div className="w-30 h-30 md:w-80 md:h-80 shadow-2xl">
-        <img
-          src={foto}
-          className="w-full h-full pr-10 transition-all duration-500 ease-in-out transform hover:scale-110 hover:brightness-90"
-          alt="joelyzoe"
-        />
+      {/* Contenedor de la imagen con padding derecho */}
+      <div className="shadow-2xl pr-6">
+      <div className="relative w-30 h-30 md:w-80 md:h-80">
+          <img
+            src={foto}
+            className=" pr-6 w-full h-full rounded-lg shadow-lg transition-transform duration-500 ease-in-out transform hover:scale-105"
+            alt="joelyzoe"
+          />
+        </div>
       </div>
     </div>
   );
